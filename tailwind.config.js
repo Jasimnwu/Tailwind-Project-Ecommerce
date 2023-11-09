a0f8377fb75f5a4d77e45ab893277cd9ce0174b4
Jasimnwu <jasimnwu75@gmail.com>
2023-11-09 14:02:32 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      'sm': '320px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1320px'
+    },
+    container: {
+      center: true,
+      padding:'1rem'
+    },
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif;"
+      },
+      colors: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
